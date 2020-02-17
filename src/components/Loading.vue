469561e377cb2fc8a190a6986bb76b1dd994d238
Johnson Mwakazi <kemsjunior99@gmail.com>
2020-02-17 10:42:59 +0300

    Update Loading.vue

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="d-flex justify-content-center mb-3">
-      <b-spinner style="width: 4rem; height: 4rem;" variant="primary" label="Loading..."></b-spinner>
+      <b-spinner style="width: 4rem; height: 4rem;" variant="primary" label="Loading"></b-spinner>
     </div>
   </div>
 </template>
